2e2f6ced5b0869c06651c3a332d3021bbc18105c
김수환 <soohwan4030@gmail.com>
2024-04-16 15:55:28 +0900

    chore: 프로젝트 기본 구조 변경

--- pydata/app/main.py
+++ pydata/app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(docs_url='/api/data/docs', redoc_url='/api/data/redoc')
+
+router = APIRouter(prefix="/api/data")
+
 
 # main은 깔끔하게 사용하기
 # main은 routers에서 호출만!!!!!!!
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
+@router.get("/test")
+def test():
+    return {"message": "this is just test"}
+
+app.include_router(router)
