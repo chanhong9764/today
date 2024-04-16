2b1f31ed3dea329353ae401af34928018f7b35af
김수환 <soohwan4030@gmail.com>
2024-04-16 14:33:45 +0900

    chore: 프로젝트 기본 구조 변경

--- pydata/app/main.py
+++ pydata/app/main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# main은 깔끔하게 사용하기
+# main은 routers에서 호출만!!!!!!!
 
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-  return {"message": f"Hello {name}"}
