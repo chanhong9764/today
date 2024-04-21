3af3961eefc0c52f5e5c8ecbde3ad5b3d4d44553
박은미 <amy0231@naver.com>
2024-04-21 18:46:03 +0900

    feat(router): bottom navigation router 연결

--- front/today/src/navigator/Navigator.tsx
+++ front/today/src/navigator/Navigator.tsx
@@ -1,5 +1,27 @@
+// Navigaior.tsx
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 
-export default function Navigator() {
-  return <div></div>;
+import Calendar from '../screens/calendar/Calendar';
+import DiaryList from '../screens/diary/DiaryList';
+import Mypage from '../screens/user/Mypage';
+
+type BottomTabParamList = {
+  Calendar: undefined;
+  DiaryList: undefined;
+  Mypage: undefined;
+};
+
+const Tab = createBottomTabNavigator<BottomTabParamList>();
+
+function Navigaior() {
+  return (
+    <Tab.Navigator initialRouteName="Calendar">
+      <Tab.Screen name="Calendar" component={Calendar} />
+      <Tab.Screen name="DiaryList" component={DiaryList} />
+      <Tab.Screen name="Mypage" component={Mypage} />
+    </Tab.Navigator>
+  );
 }
+
+export default Navigaior;
--- front/today/src/screens/user/Mypage.tsx
+++ front/today/src/screens/user/Mypage.tsx
@@ -1,1 +1,12 @@
-function MyPage() {}
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Mypage() {
+  return (
+    <View>
+      <Text>마이페이지 입니다.</Text>
+    </View>
+  );
+}
+
+export default Mypage;
