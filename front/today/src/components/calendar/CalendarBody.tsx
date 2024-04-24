cf88e3854e5e5970695397ac4a1b53542ac43edf
박은미 <amy0231@naver.com>
2024-04-24 12:31:26 +0900

    feat(calendar): calendar data select

--- front/today/src/components/calendar/CalendarBody.tsx
+++ front/today/src/components/calendar/CalendarBody.tsx
@@ -1,1 +1,37 @@
-function CalendarBody() {}
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { StyleSheet, ViewStyle } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+interface Styles {
+  calendar: ViewStyle;
+}
+
+const CalendarBody = () => {
+  // // 선택 날짜 상태 관리
+  const [selected, setSelected] = useState('');
+  const navigation = useNavigation();
+
+  return (
+    <Calendar
+      onDayPress={day => {
+        setSelected(day.dateString);
+        // navigation.na`vigate('DiaryList', { selectedDate: day.dateString });
+      }}
+      markedDates={{
+        [selected]: { selected: true, selectedColor: '#ff7d7d' },
+      }}
+      style={{ height: '100%' }}
+    />
+  );
+};
+
+// StyleSheet 정의
+const styles = StyleSheet.create<Styles>({
+  calendar: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#e0e0e0',
+  },
+});
+
+export default CalendarBody;
--- front/today/src/components/calendar/Month.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-function Month() {
-  return <div></div>;
-}
