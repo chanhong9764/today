04f2cfde19b5e7286065414cb3f51a8e127c197c
chanhong9784 <chanhong9784@naver.com>
2024-04-24 16:03:48 +0900

    feat(sdxl): base 추가

--- pydata/app/main.py
+++ pydata/app/main.py
@@ -9,10 +9,13 @@ app = FastAPI(docs_url='/api/data/docs', redoc_url='/api/data/redoc')
 
 @app.on_event("startup")
 def startup():
+    global base
     # 초기 환경변수 설정
     load_env()
     # 초기 Stable Diffusion XL 설정
-    load_sdxl()
+    base = load_sdxl()
+    print(base)
+
 
 # main은 깔끔하게 사용하기
 # main은 routers에서 호출만!!!!!!!
