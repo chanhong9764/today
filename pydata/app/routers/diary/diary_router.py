5ef84e86867870520156c45291d7d4828cb6ddf4
chanhong9784 <chanhong9784@naver.com>
2024-04-26 08:52:42 +0900

    feat(sdxl): 그림 저장

--- pydata/app/routers/diary/diary_router.py
+++ pydata/app/routers/diary/diary_router.py
@@ -2,13 +2,13 @@ import app.utils.global_vars
 
 from fastapi import APIRouter
 from pathlib import Path
-from app.services.diary.diary_image import create_image
+from app.services.diary.diary_image import create_image, diary_translate
 from app.routers.diary.diary_schema import createImageDto
 
 router = APIRouter(prefix="/api/data/diary")
 
 
-@router.post("/")
+@router.post("")
 def make_image(data: createImageDto):
-    print(data.prompt)
-    #create_image()
+    keyword = diary_translate(data.prompt)
+    create_image(keyword)
--- pydata/app/services/diary/diary_image.py
+++ pydata/app/services/diary/diary_image.py
@@ -1,35 +1,23 @@
 import app.utils.global_vars
 from pathlib import Path
+from dotenv import load_dotenv
+import openai
+import os
 
-def create_image():
+load_dotenv()
+
+def create_image(prompt):
     # 모델 설정
     base = app.utils.global_vars.base
-
-    
     # 이미지 타입
-    types = ["childrens_book_illustration, ", "Aardman Animations Style page, ", "pixel art, 64 bit,", ""]
+    types = ["childrens_book_illustration, ", "Aardman Animations Style page, ", "pixel art, 64 bit, ", "realistic, "]
     # Lora 타입
     lora_types = ["child", "animate", "pixel", "detail"]
     # 추론 횟수
-    n_steps = 30
+    n_steps = 40
     # 부정 프롬프트
     negative_prompt = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face, b&w, watermark EasyNegative"
-    # 프롬프트
-    prompt = "an asian man"
     
-    save_path = Path("/home/j-k10b108/image")
-    
-    image = base(
-            prompt=types[0] + prompt,
-            guidance_scale=7,
-            negative_prompt=negative_prompt,
-            num_inference_steps=n_steps,
-        ).images[0]
-
-    file_name = "image.png"
-    file_path = save_path / file_name
-    image.save(file_path)
-    """
     images = []
 
     save_path = Path("/home/j-k10b108/image")
@@ -48,4 +36,28 @@ def create_image():
         file_name = f"image_{i}.png"
         file_path = save_path / file_name
         image.save(file_path)
-    """
+
+def diary_translate(content):
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    client = openai.OpenAI(api_key=openai_api_key)
+    
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Stable Diffusion is an AI art generation model similar to DALLE-2."},
+{"role": "system", "content": "I'm going to put it in the prompt of the Stable Diffusion model"},
+{"role": "system", "content": "Please let me know the answer that satisfies the rules below"},
+{"role": "system", "content": "- Please don't transform the text you entered"},
+{"role": "system", "content": "- Please translate Korean into English"},
+{"role": "system", "content": "- Please extract it in a keyword format, not a sentence format"},
+{"role": "system", "content": "- Extract the key words from the translation and distribute the words with commas"},
+{"role": "system", "content": "- I'm going to put it in the prompt of the Stable Diffusion model, so change the format according to the prompt"},
+{"role": "system", "content": "- Just write the keywords and let me know"},
+{"role": "system", "content": "I want you to write me a list of detailed prompts exactly about the idea written after IDEA."},
+{"role": "user", "content": "IDEA:" +  content}
+        ]
+
+    )
+    return completion.choices[0].message.content
+
+    
