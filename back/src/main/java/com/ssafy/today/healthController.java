c100c1a607f233ea89854cefef9b3c5f42e0695c
최준영 <wjdghkswnsdu@naver.com>
2024-05-02 17:00:19 +0900

    feat(Infra): 헬스 체크 로그

--- back/src/main/java/com/ssafy/today/healthController.java
+++ back/src/main/java/com/ssafy/today/healthController.java
@@ -4,17 +4,20 @@ import static com.ssafy.today.util.response.SuccessResponseEntity.getResponseEnt
 
 import com.ssafy.today.util.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/health")
 public class healthController {
   @GetMapping("")
   public ResponseEntity<?> getStatus(){
+    log.info("receive health check request");
     return getResponseEntity(SuccessCode.OK, "isWorking");
   }
 }
