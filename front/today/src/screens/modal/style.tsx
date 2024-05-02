eb965fafa6e89ada57aedfbaf7edb209f4fc288f
김유정 <kimyujung55@naver.com>
2024-05-02 15:02:27 +0900

    feat(diary): 일기 분석 결과 모달 생성

--- front/today/src/screens/modal/style.tsx
+++ front/today/src/screens/modal/style.tsx
@@ -13,3 +13,29 @@ export const MakingDiaryContainer = styled.View`
 export const MakingDiaryText = styled.Text`
   text-align: center;
 `;
+
+export const ResultModalContainer = styled.View`
+  margin: 10px;
+`;
+
+export const ResultModalTextContainer = styled.View`
+  ${center}
+  margin-bottom: 20px;
+`;
+export const ResultModalTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  margin-bottom: 8px;
+`;
+
+export const ResultModalText = styled.Text`
+  font-size: ${({ theme }) => theme.fontSize.regular};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  margin: 6px;
+`;
+
+export const ResultModalPoiotText = styled.Text`
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  color: ${({ theme }) => theme.colors.pink};
+`;
--- front/today/src/screens/user/Mypage.tsx
+++ front/today/src/screens/user/Mypage.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, Text } from 'react-native';
+import { Members } from '../../apis/MemberApi';
 import Graph from '../../components/user/Graph';
+import { MemberData } from '../../types/datatype';
 import { UserProp } from '../../types/navigatortype/stack';
+import ResultModal from '../modal/Result';
 import * as S from './style';
 
 function Mypage({ navigation }: UserProp) {
-  // useEffect(() => {
-  //   Members.getMembers()
-  //     .then(res => {})
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // });
+  const [memberInfo, setMemberInfo] = useState<MemberData[]>([]);
 
+  useEffect(() => {
+    Members.getMembers()
+      .then(res => {
+        setMemberInfo(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  const [resultOpen, setResultOpen] = useState<boolean>(false);
   return (
-    <S.MyPageContainer>
-      <S.MyPageTitle>MY PAGE</S.MyPageTitle>
-      <S.MyPageSubTitle>MY INFO</S.MyPageSubTitle>
-      <S.MyInfo>
-        <S.MyInfoTitle>닉네임</S.MyInfoTitle>
-        <S.MyInfoContent>
-          <Text>potato</Text>
-        </S.MyInfoContent>
-      </S.MyInfo>
-      <S.MyInfo>
-        <S.MyInfoTitle>이메일</S.MyInfoTitle>
-        <S.MyInfoContent>
-          <Text>potato@naver.com</Text>
-        </S.MyInfoContent>
-      </S.MyInfo>
+    <S.MyPage>
+      <S.MyPageContainer>
+        <S.MyPageSubTitle>MY INFO</S.MyPageSubTitle>
+        <S.MyInfo>
+          <S.MyInfoTitle>닉네임</S.MyInfoTitle>
+          <S.MyInfoContent>
+            <Text>{memberInfo[0]?.nickname}</Text>
+          </S.MyInfoContent>
+        </S.MyInfo>
+        <S.MyInfo>
+          <S.MyInfoTitle>이메일</S.MyInfoTitle>
+          <S.MyInfoContent>
+            <Text>{memberInfo[0]?.email}</Text>
+          </S.MyInfoContent>
+        </S.MyInfo>
 
-      <S.MyPageSubTitle>김유정 님의 성향은</S.MyPageSubTitle>
-      <S.MyPageSubTitle>김유정 님의 감정은</S.MyPageSubTitle>
-      <Graph
-        labels={['행복', '슬픔', '분노', '짜증', '불안', '놀람']}
-        data={[8, 7, 9, 5, 6, 10]}
-        chartTitle="나의 감정통계"
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('KakaoLogin')}>
-        <Text>버튼</Text>
-      </TouchableOpacity>
-    </S.MyPageContainer>
+        <S.MyPageSubTitle>김유정 님의 성향은</S.MyPageSubTitle>
+        <S.MyPageSubTitle>김유정 님의 감정은</S.MyPageSubTitle>
+        <Graph
+          labels={['행복', '슬픔', '분노', '짜증', '불안', '놀람']}
+          data={[8, 7, 9, 5, 6, 10]}
+          chartTitle="나의 감정통계"
+        />
+        <Button title="카카오 로그인 버튼" onPress={() => navigation.navigate('KakaoLogin')} />
+
+        <Button title="분석 결과 모달" onPress={() => setResultOpen(!resultOpen)} />
+        <ResultModal modalVisible={resultOpen} setModalVisible={setResultOpen} />
+      </S.MyPageContainer>
+    </S.MyPage>
   );
 }
 
