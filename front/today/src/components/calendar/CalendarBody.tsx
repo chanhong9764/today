91e1856ace6ca244778e52f5290d251dbf019edd
박은미 <amy0231@naver.com>
2024-05-02 14:56:22 +0900

    setting(api): calendar api 연결

--- front/today/src/components/calendar/CalendarBody.tsx
+++ front/today/src/components/calendar/CalendarBody.tsx
@@ -1,8 +1,10 @@
-// // CalendarBody.tsx
+// CalendarBody.tsx
 import { useEffect, useState } from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+// import { Calendars } from '../../apis/CalendarApi';
+// import { DiaryData } from '../../types/diary';
 import dummy from '../../db/data.json';
 import { Header, PressedDate, dayType } from '../../types/calendar';
+import * as S from './style';
 
 const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
@@ -107,102 +109,181 @@ const CalendarBody = ({
 
   return (
     <>
-      <View style={styles.dayOfWeek}>
-        {dayOfWeek.map((day, index) => (
-          <View style={styles.box} key={index}>
-            <Text style={dayOfWeekString(day).styled}>{day}</Text>
-          </View>
+      <S.DayOfWeek>
+        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day, index) => (
+          <S.Box key={index}>
+            <S.DayText day={day}>{day}</S.DayText>
+          </S.Box>
         ))}
-      </View>
-      <View style={styles.totalDays}>
+      </S.DayOfWeek>
+      <S.TotalDays>
         {Object.keys(totalDays).map((state: string) =>
           totalDays[state].daysList.map((day: number, index: number) => {
             const dateString = `${totalDays[state].year}-${('0' + totalDays[state].month).slice(-2)}-${('0' + day).slice(-2)}`;
             const imageUrl = state === 'curr' ? imageUrlByDate[dateString] : undefined;
-            // Determine the text color based on the presence of an image
             const textColor = imageUrl ? '#FFFFFF' : state === 'curr' ? '#4A4A4A' : 'lightgray';
             return (
-              <View style={styles.dayBoxContainer} key={index}>
+              <S.DayBoxContainer key={index}>
                 {imageUrl ? (
-                  <ImageBackground source={{ uri: imageUrl }} style={styles.imageBackground} resizeMode="cover">
-                    <Text style={[styles.dateText, { color: textColor }]}>{day}</Text>
-                  </ImageBackground>
+                  <S.ImageBackgroundStyled source={{ uri: imageUrl }}>
+                    <S.DateText textColor={textColor}>{day}</S.DateText>
+                  </S.ImageBackgroundStyled>
                 ) : (
-                  <Text style={[styles.dateText, { color: textColor }]}>{day}</Text>
+                  <S.DateText textColor={textColor}>{day}</S.DateText>
                 )}
-              </View>
+              </S.DayBoxContainer>
             );
           }),
         )}
-      </View>
+      </S.TotalDays>
     </>
   );
 };
 
-const dayOfWeekString = (day: string) =>
-  StyleSheet.create({
-    styled: {
-      color: day === 'Sun' ? 'tomato' : day === 'Sat' ? 'royalblue' : 'gray',
-    },
-  });
-
-const styles = StyleSheet.create({
-  dayOfWeek: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 5,
-    marginTop: 15,
-  },
-  // 날짜 요일 칸
-  box: {
-    width: '14.2%',
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  totalDays: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  dayBox: {
-    width: '14.2%', // 전체 컨테이너 너비의 1/7
-    height: 100, // 각 날짜 칸의 높이
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden', // 이미지가 박스를 넘어가지 않도록
-  },
-  pressable: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  dayBoxContainer: {
-    width: '14.2%',
-    height: 90,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imageBackground: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  prev: {
-    color: 'lightgray',
-    fontWeight: '300',
-    fontSize: 16,
-  },
-  next: {
-    color: 'lightgray',
-    fontWeight: '300',
-    fontSize: 16,
-  },
-  dateText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
-
 export default CalendarBody;
+
+// import React, { useEffect, useState } from 'react';
+// import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+// import { Header, PressedDate, dayType } from '../../types/calendar';
+
+// interface CalendarBodyProps extends Header {
+//   diaryData: any[];
+// }
+
+// const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// // const isSameObj = (obj1: PressedDate, obj2: PressedDate) => {
+// //   return JSON.stringify(obj1) === JSON.stringify(obj2);
+// // };
+
+// const CalendarBody = ({
+//   month,
+//   year,
+//   date,
+//   today,
+//   diaryData,
+//   moveToNextMonth,
+//   moveToPreviousMonth,
+//   moveToSpecificYearAndMonth
+// }: CalendarBodyProps) => {
+
+//  //날짜별 이미지 URL 매핑 객체 생성
+//   const imageUrlByDate: { [key: string]: string } = {};
+//   diaryData.forEach(data => {
+//     const date = new Date(data.createdAt);
+//     const key = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+//     imageUrlByDate[key] = data.imgUrl;
+//   });
+
+//   const initialState = {
+//     prev: {
+//       daysList: [],
+//       year: 0,
+//       month: 0,
+//     },
+//     curr: {
+//       daysList: [],
+//       year: 0,
+//       month: 0,
+//     },
+//     next: {
+//       daysList: [],
+//       year: 0,
+//       month: 0,
+//     },
+//   };
+
+//   const [totalDays, setTotalDays] = useState<dayType>(initialState);
+//   const [pressedDate, setPressedDate] = useState({
+//     state: '',
+//     year: 0,
+//     month: 0,
+//     date: 0,
+//   });
+
+//   // 연도와 월에 따른 전달, 현재달, 다음달 일수와 정보 세팅
+//   const getTotalDays = (year: number, month: number) => {
+//     const previousMonthLastDate = new Date(year, month - 1, 0).getDate(); // 이전 달의 마지막 날짜 체크
+//     const previousMonthLastDay = new Date(year, month - 1, 0).getDay(); // 이전 달의 마지막 날짜의 요일
+//     const currentMonthLastDate = new Date(year, month, 0).getDate();
+//     const currentMonthLastDay = new Date(year, month, 0).getDay();
+
+//     const previousDays = [];
+//     for (let i = 0; i < previousMonthLastDay + 1; i++) {
+//       previousDays.push(previousMonthLastDate - previousMonthLastDay + i);
+//     }
+
+//     const currentDays = [];
+//     for (let i = 0; i < currentMonthLastDate; i++) {
+//       currentDays.push(i + 1);
+//     }
+
+//     const nextDays = [];
+//     for (let i = 0; i < 6 - currentMonthLastDay; i++) {
+//       nextDays.push(i + 1);
+//     }
+
+//     setTotalDays({
+//       prev: {
+//         daysList: previousMonthLastDay !== 6 ? previousDays : [],
+//         year: month === 1 ? year - 1 : year,
+//         month: month === 1 ? 12 : month - 1,
+//       },
+//       curr: {
+//         daysList: currentDays,
+//         year: year,
+//         month: month,
+//       },
+//       next: {
+//         daysList: nextDays,
+//         year: month === 12 ? year + 1 : year,
+//         month: month === 12 ? 1 : month + 1,
+//       },
+//     });
+//   };
+
+//   const handlePressDay = (pressedDate: PressedDate) => {
+//     setPressedDate(pressedDate);
+//     if (pressedDate.state === 'prev' || pressedDate.state === 'next') {
+//       moveToSpecificYearAndMonth(pressedDate.year, pressedDate.month);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getTotalDays(year, month);
+//   }, [year, month, date]);
+
+//   return (
+//     <>
+//      <S.DayOfWeek>
+//      {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day, index) => (
+//        <S.Box key={index}>
+//         <S.DayText day={day}>{day}</S.DayText>
+//        </S.Box>
+//      ))}
+//      </S.DayOfWeek>
+//       <S.TotalDays>
+//         {Object.keys(totalDays).map((state: string) =>
+//           totalDays[state].daysList.map((day: number, index: number) => {
+//             const dateString = `${totalDays[state].year}-${('0' + totalDays[state].month).slice(-2)}-${('0' + day).slice(-2)}`;
+//             const imageUrl = state === 'curr' ? imageUrlByDate[dateString] : undefined;
+//             const textColor = imageUrl ? '#FFFFFF' : state === 'curr' ? '#4A4A4A' : 'lightgray';
+//             return (
+//               <S.DayBoxContainer key={index}>
+//                 {imageUrl ? (
+//                   <S.ImageBackgroundStyled source={{ uri: imageUrl }}>
+//                     <S.DateText textColor={textColor}>{day}</S.DateText>
+//                   </S.ImageBackgroundStyled>
+//                 ) : (
+//                   <S.DateText textColor={textColor}>{day}</S.DateText>
+//                 )}
+//               </S.DayBoxContainer>
+//             );
+//           }),
+//         )}
+//       </S.TotalDays>
+//     </>
+//   );
+// };
+
+// export default CalendarBody;
--- front/today/src/screens/calendar/Calendar.tsx
+++ front/today/src/screens/calendar/Calendar.tsx
@@ -1,10 +1,13 @@
+// Calendar.tsx
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import AddButton from '../../common/AddButton';
+import { CalendarProp } from '../../types/stack';
+// import { Calendars } from '../../apis/CalendarApi';
+// import { DiaryData } from '../../types/diary';
 
+import AddButton from '../../common/AddButton';
 import CalendarBody from '../../components/calendar/CalendarBody';
 import CalendarHeader from '../../components/calendar/CalendarHeader';
-import { CalendarProp } from '../../types/stack';
 
 function Calendar({ navigation }: CalendarProp) {
   // 일기 생성 페이지로 이동
@@ -78,3 +81,103 @@ const styles = StyleSheet.create({
     // backgroundColor: 'white',
   },
 });
+
+// import React, { useEffect, useState } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { Calendars } from '../../apis/CalendarApi';
+// import { DiaryData } from '../../types/diary';
+// import { CalendarProp } from '../../types/stack';
+
+// import AddButton from '../../common/AddButton';
+// import CalendarBody from '../../components/calendar/CalendarBody';
+// import CalendarHeader from '../../components/calendar/CalendarHeader';
+
+// function Calendar({ navigation }: CalendarProp) {
+//   // 일기 생성 페이지로 이동
+//   function navigateToWrite() {
+//     navigation.push('SelectEmotion');
+//   }
+
+//   const [month, setMonth] = useState(new Date().getMonth() + 1);
+//   const [year, setYear] = useState(new Date().getFullYear());
+//   const [diaryData, setDiaryData] = useState<DiaryData[]>([]);
+
+//   // 오늘의 날짜를 계산
+//   const today = {
+//     year: new Date().getFullYear(),
+//     month: new Date().getMonth() + 1,
+//     date: new Date().getDate(),
+//   };
+
+//   // API에서 데이터를 가져오는 함수
+//   const fetchDiaryData = async () => {
+//     const formattedMonth = `${year}-${('0' + month).slice(-2)}`;
+//     try {
+//       const data = await Calendars.getCalendars(formattedMonth);
+//       setDiaryData(Array.isArray(data) ? data : [data]); // 데이터 상태 업데이트
+//     } catch (error) {
+//       console.error('월 데이터 요청 실패', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchDiaryData();
+//   }, [month, year]);
+
+//   const moveToNextMonth = () => {
+//     if (month === 12) {
+//       setYear(year + 1);
+//       setMonth(1);
+//     } else {
+//       setMonth(month + 1);
+//     }
+//   };
+
+//   const moveToPreviousMonth = () => {
+//     if (month === 1) {
+//       setYear(year - 1);
+//       setMonth(12);
+//     } else {
+//       setMonth(month - 1);
+//     }
+//   };
+
+//   const moveToSpecificYearAndMonth = (newYear: number, newMonth: number) => {
+//     setYear(newYear);
+//     setMonth(newMonth);
+//   };
+
+//   return (
+//     <View style={styles.calendarContainer}>
+//       <AddButton onPress={navigateToWrite} />
+//       <CalendarHeader
+//         month={month}
+//         year={year}
+//         date={today.date}
+//         today={today}
+//         moveToNextMonth={moveToNextMonth}
+//         moveToPreviousMonth={moveToPreviousMonth}
+//         moveToSpecificYearAndMonth={moveToSpecificYearAndMonth}
+//       />
+//       <CalendarBody
+//         month={month}
+//         year={year}
+//         diaryData={diaryData}
+//         date={today.date}
+//         today={today}
+//         moveToNextMonth={moveToNextMonth}
+//         moveToPreviousMonth={moveToPreviousMonth}
+//         moveToSpecificYearAndMonth={moveToSpecificYearAndMonth}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   calendarContainer: {
+//     margin: 10,
+//     // backgroundColor: 'white',
+//   },
+// });
+
+// export default Calendar;
