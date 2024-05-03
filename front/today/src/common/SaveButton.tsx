61ae260ba6015ce38c2e95443b68762e0512709f
박은미 <amy0231@naver.com>
2024-05-03 16:06:36 +0900

    feat(diary): save button 수정

--- front/today/src/common/SaveButton.tsx
+++ front/today/src/common/SaveButton.tsx
@@ -1,6 +1,6 @@
+import * as FileSystem from 'expo-file-system';
 import React from 'react';
-import { Button, Platform } from 'react-native';
-import RNFS from 'react-native-fs'; // 파일 시스템 접근을 위한 라이브러리
+import { Button } from 'react-native';
 
 export type SaveButtonProps = {
   onPress?: (data?: any) => void;
@@ -9,25 +9,24 @@ export type SaveButtonProps = {
 
 function SaveButton({ onPress, viewShotRef }: SaveButtonProps) {
   const captureAndSavePhoto = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const uri: string = await viewShotRef.current.capture();
-        const fileName: string = `screenshot_${Date.now()}.jpg`;
-        const destPath: string = `${RNFS.PicturesDirectoryPath}/${fileName}`;
+    try {
+      const uri = await viewShotRef.current.capture();
+      const fileName = `screenshot_${Date.now()}.jpg`;
+      const destPath = `${FileSystem.documentDirectory}${fileName}`;
 
-        // 이미지 파일을 PicturesDirectoryPath로 이동
-        await RNFS.moveFile(uri, destPath);
+      // 이미지 파일을 documentDirectory로 이동
+      await FileSystem.moveAsync({
+        from: uri,
+        to: destPath,
+      });
 
-        console.log('이미지 이동', destPath);
-        if (onPress) onPress(destPath);
-      } catch (error) {
-        console.error('이미지 저장 실패', error);
-      }
-    } else {
-      console.log('이미지 저장 성공');
-      if (onPress) onPress();
+      console.log('이미지 저장', destPath);
+      if (onPress) onPress(destPath);
+    } catch (error) {
+      console.error('이미지 저장 실패', error);
     }
   };
+
   return <Button title="Save Diary" onPress={captureAndSavePhoto} />;
 }
 
