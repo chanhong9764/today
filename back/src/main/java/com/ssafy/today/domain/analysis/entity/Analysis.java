d9a9a200a33f146e450076190ba8e0abda23e14b
한성현 <tjdgus4560@naver.com>
2024-05-03 16:12:10 +0900

    feat(Diary) : Analysis api 기본구조 생성

--- back/src/main/java/com/ssafy/today/domain/analysis/entity/Analysis.java
+++ back/src/main/java/com/ssafy/today/domain/analysis/entity/Analysis.java
@@ -24,9 +24,67 @@ public class Analysis extends BaseTimeEntity {
     private Member member;
 
     @ColumnDefault("0")
-    private int count;
+    private Integer count;
 
+    @ColumnDefault("0")
+    private Integer e;
 
+    @ColumnDefault("0")
+    private Integer i;
 
+    @ColumnDefault("0")
+    private Integer s;
 
+    @ColumnDefault("0")
+    private Integer n;
+
+    @ColumnDefault("0")
+    private Integer f;
+
+    @ColumnDefault("0")
+    private Integer t;
+
+    @ColumnDefault("0")
+    private Integer p;
+
+    @ColumnDefault("0")
+    private Integer j;
+
+    @ColumnDefault("0")
+    private Double angry;
+
+    @ColumnDefault("0")
+    private Double disgust;
+
+    @ColumnDefault("0")
+    private Double fear;
+
+    @ColumnDefault("0")
+    private Double happiness;
+
+    @ColumnDefault("0")
+    private Double sadness;
+
+    @ColumnDefault("0")
+    private Double surprise;
+
+    @Builder
+    public Analysis(Member member, Integer count, Integer e, Integer i, Integer s, Integer n, Integer f, Integer t, Integer p, Integer j, Double angry, Double disgust, Double fear, Double happiness, Double sadness, Double surprise) {
+        this.member = member;
+        this.count = count;
+        this.e = e;
+        this.i = i;
+        this.s = s;
+        this.n = n;
+        this.f = f;
+        this.t = t;
+        this.p = p;
+        this.j = j;
+        this.angry = angry;
+        this.disgust = disgust;
+        this.fear = fear;
+        this.happiness = happiness;
+        this.sadness = sadness;
+        this.surprise = surprise;
+    }
 }
