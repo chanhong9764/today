03c60a40d2384751c36832c1c06f77cad59c5aaa
한성현 <tjdgus4560@naver.com>
2024-05-08 12:24:24 +0900

    fix(img) : import TempImg

--- back/src/main/java/com/ssafy/today/domain/diary/dto/request/DiaryContentCreated.java
+++ back/src/main/java/com/ssafy/today/domain/diary/dto/request/DiaryContentCreated.java
@@ -3,6 +3,7 @@ package com.ssafy.today.domain.diary.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.today.domain.diary.entity.Diary;
 import com.ssafy.today.domain.member.entity.Member;
+import com.ssafy.today.domain.tempimg.entity.TempImg;
 import lombok.Builder;
 import lombok.Data;
 
