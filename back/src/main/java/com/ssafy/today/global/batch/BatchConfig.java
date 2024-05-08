bb24526d5776c328c885133f5ba199352e1ee261
최준영 <wjdghkswnsdu@naver.com>
2024-05-08 12:26:59 +0900

    feat(notice): batch step 및 tasklet 추가

--- back/src/main/java/com/ssafy/today/global/batch/BatchConfig.java
+++ back/src/main/java/com/ssafy/today/global/batch/BatchConfig.java
@@ -1,6 +1,10 @@
 package com.ssafy.today.global.batch;
 
 
+import com.ssafy.today.domain.member.entity.Member;
+import com.ssafy.today.domain.member.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -13,15 +17,16 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
-
+@Slf4j
 @Configuration
 @EnableBatchProcessing
-@Slf4j
+@RequiredArgsConstructor
 public class BatchConfig {
+    private final MemberRepository memberRepository;
     @Bean
-    public Job Job1(JobRepository jobRepository, Step simpleStep1) {
+    public Job Job1(JobRepository jobRepository, Step Step1) {
         return new JobBuilder("Job1", jobRepository)
-                .start(simpleStep1)
+                .start(Step1)
                 .build();
     }
     @Bean
@@ -30,12 +35,17 @@ public class BatchConfig {
                 .tasklet(Tasklet, platformTransactionManager).build();
     }
     @Bean
-    public Tasklet Tasklet(){
+    public Tasklet Tasklet() {
         return ((contribution, chunkContext) -> {
-            log.info("step1 지납니다~");
-            // 알아서 Tasklet1 을 찾아간다
+            log.info("Job1 시작한다 ~");
+            // 금일 일기를 작성하지 않은 멤버들을 뽑아온다.
+            List<Member> members = memberRepository.findYetWrite();
+            for (Member tmp : members) {
+                System.out.println(tmp);
+            }
+            // 알림을 호출한다.
+
             return RepeatStatus.FINISHED;
         });
     }
-
 }
