fe7778418f3d1cf27b6e1c0ed4f90d4a2cf7c75c
박은미 <amy0231@naver.com>
2024-05-09 23:25:58 +0900

    feat(diary): 하루 통계 api 연결

--- front/today/src/screens/modal/analysis/Analysis.tsx
+++ front/today/src/screens/modal/analysis/Analysis.tsx
@@ -1,20 +1,36 @@
+// Analysis.tsx
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import { analysis } from '../../../apis/AnalysisApi';
 import ModalComponent from '../../../components/modal/Modal';
 import Graph from '../../../components/user/Graph';
 import Pyramid from '../../../components/user/Pyramid';
+import { AnalysisData } from '../../../types/datatype';
 import { ModalProps } from '../../../types/modal';
 import * as S from './style';
 
 function AnalysisContent() {
+  const [analysisData, setAnalysisData] = useState<AnalysisData | undefined>(undefined);
+
+  useEffect(() => {
+    const today = new Date().toISOString().slice(0, 10); // YYYY-MM-DD 형식으로 변환
+    analysis
+      .getAnalysisday(today) // 하루치 데이터 불러오기
+      .then(response => {
+        setAnalysisData(response.data); // 데이터 상태 업데이트
+      })
+      .catch(error => {
+        console.error('Error fetching analysis data:', error);
+      });
+  }, []);
+
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
       <S.AnalysisTitle>오늘의 일기 분석 결과</S.AnalysisTitle>
       <S.AnalysisSubTitle>김유정님의 오늘의 성격유형입니다</S.AnalysisSubTitle>
-      <Pyramid height={150} width={250} />
+      {analysisData && <Pyramid analysisData={analysisData} width={280} height={250} />}
       <S.AnalysisSubTitle>김유정님의 오늘의 기분입니다</S.AnalysisSubTitle>
-      <View style={{ height: 280, width: 280 }}>
-        <Graph data={[8, 7, 9, 5, 6, 10]} />
-      </View>
+      <View style={{ height: 280, width: 280 }}>{analysisData && <Graph analysisData={analysisData} />}</View>
     </View>
   );
 }
