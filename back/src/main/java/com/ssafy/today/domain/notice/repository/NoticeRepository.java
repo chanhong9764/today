0c74b5fdb091193acc2da9948b9b4a147dddb65b
최준영 <wjdghkswnsdu@naver.com>
2024-05-09 17:43:39 +0900

    feat(notice): notice response Api define

--- back/src/main/java/com/ssafy/today/domain/notice/repository/NoticeRepository.java
+++ back/src/main/java/com/ssafy/today/domain/notice/repository/NoticeRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-  List<Notice> findAllByMember(Member member);
+  List<Notice> findAllByMemberOrderByCreatedAtDesc(Member member);
 }
