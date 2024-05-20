4ace5e7540e72866e3cb979ddef1dd7af9d588b8
chanhong9784 <chanhong9784@naver.com>
2024-05-20 11:00:17 +0900

    fix(project): 버전 수정 및 멘트 삭제

--- front/today/src/screens/diary/wait/WaitImage.tsx
+++ front/today/src/screens/diary/wait/WaitImage.tsx
@@ -48,7 +48,6 @@ function WaitImage({ navigation }: WaitImageProp) {
         <S.WaitImageContent>오늘 하루를 그리는 중입니다.</S.WaitImageContent>
         <S.WaitImageContent>잠시만 기다려주세요.</S.WaitImageContent>
 
-        <S.WaitImageExplane>그림을 생성하는데 약 3분 정도 소요됩니다.</S.WaitImageExplane>
         <S.WaitImageExplane>그림 생성이 완료되면 알림을 보내드려요!</S.WaitImageExplane>
         <S.ButtonContainer>
           <CommonButton content="홈으로 돌아가기" onPress={onPressGoHome} />
